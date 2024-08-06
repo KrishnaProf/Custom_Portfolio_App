db0113d2f3c58f9467ad6fc9f2e3a54b6c453475
Krishna <leelakrishna682@gmail.com>
2024-08-06 10:31:18 +0530

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,4 +1,4 @@
-const Contact = () => {
+const Contact = () =>  {
   return (
     <div id="contact" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
       <h1 className="py-4 text-4xl font-bold text-center text-[#001b5e]">
